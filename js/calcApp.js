e6063f5ec2a9da66bd7d910d5aed19d268b85f0a
Louis Medina <Louisfm16@gmail.com>
2021-08-17 21:10:37 -0700

    Refactored HandleOperatorClick

--- js/calcApp.js
+++ js/calcApp.js
@@ -1,7 +1,7 @@
 import { helpers as h } from './helpers.js';
 
 export let CalcApp = (function () {
-  //#region [Standard] Variables
+  //#region Variables
   // -- Elements
   let appEl,
     dispEvalEl,
@@ -15,7 +15,7 @@ export let CalcApp = (function () {
   let calcs = [];
   //#endregion Variables
 
-  //#region [Standard] Setters, Getters, & Helpers
+  //#region Setters, Getters, & Helpers
   let calculator = {
     getCurrUserVal: () => currUserVal,
     setCurrUserVal: (newVal) => currUserVal = newVal,
@@ -27,34 +27,37 @@ export let CalcApp = (function () {
     setDispEvalEl: (newVal) => dispEvalEl.innerText = newVal,
     getDispEquateEl: () => dispEquateEl.innerText,
     setDispEquateEl: (newVal) => dispEquateEl.innerText = newVal,
+    getLastResult: () => !calculator.isCalcsEmpty() ? calcs[calcs.length - 1].result : 'No results Found',
+    getLastOperator: () => !calculator.isCalcsEmpty() ? calcs[calcs.length - 1].operator : 'No operators Found',
     getCalcs: () => calcs,
     isCalcsEmpty: () => h.IsEmpty(calcs)
   };
   //#endregion Setters & Getters
 
-  //#region [Standard] App Functions
-  calculator.Init = function () {
+  //#region App Functions
+  calculator.Init = function (/* app, eval, equate */) {// Adding parameters breaks this, strict mode or whatever idk
+    // appEl = document.getElementById(app);
+    // dispEvalEl = document.getElementById(eval);
+    // dispEquateEl = document.getElementById(equate);
+
     appEl = document.getElementById('app');
     dispEvalEl = document.getElementById('display--eval');
     dispEquateEl = document.getElementById('display--equation');
   }
   
   calculator.UpdateDisplay = function (val, ovrWrtStr) {
-    if (calcs.length <= 0) { // If calcs is empty, cant use operate function
-      this.UpdateEval(val);
-    } else {
-      this.UpdateEval(this.Operate(
+    if (!this.isCalcsEmpty()) { // If calcs is empty, cant use operate function
+      val = this.Operate(
         this.getCurrUserOperator(), 
-        calcs[calcs.length - 1].result, 
+        this.getLastResult(), 
         this.getCurrUserVal()
-      ));
+      );
     }
-
-    this.UpdateEquation(ovrWrtStr); // Setup for later
+    this.UpdateEval(val);
+    this.UpdateEquation(ovrWrtStr);
   }
 
   calculator.UpdateEval = function(newVal) {
-    // dispEvalEl.innerHTML = newVal;
     this.setDispEvalEl(newVal);
   }
 
@@ -62,20 +65,15 @@ export let CalcApp = (function () {
     let equateStr = '';
 
     for (let i = 0; i < calcs.length; i++) {
-      if (i == 0) {
-        equateStr += `${calcs[i].result} `;
-      } else {
-        equateStr += ` ${h.GetOperatorSymbol(calcs[i].operator)} ${calcs[i].num2} `;
-      }
+      equateStr += (i != 0) ? ` ${h.GetOperatorSymbol(calcs[i].operator)} ${calcs[i].num2} ` : `${calcs[i].result} `;
     }
 
-    equateStr += ` ${h.GetOperatorSymbol(currUserOperator)} ${currUserVal}`;
+    equateStr += ` ${h.GetOperatorSymbol(currUserOperator)} ${this.getCurrUserVal()}`;
 
-    // dispEquateEl.innerHTML = ovrWrtStr ? ovrWrtStr : equateStr;
     this.setDispEquateEl(ovrWrtStr ? ovrWrtStr : equateStr);
   }
 
-  calculator.AddCalculation = function(calc, lastOp) {
+  calculator.AddCalculation = function(calc) {
     calcs.push({ ...calc });
   }
 
@@ -86,71 +84,15 @@ export let CalcApp = (function () {
     }
   }
 
-  calculator.Reset = () => {
-    currUserVal = '';
-    currUserOperator = '';
-    lastClicked = '';
+  calculator.Reset = function() {
+    this.setCurrUserVal('');
+    this.setCurrUserOperator('');
+    this.setLastClicked('');
     calcs = [];
   }
   //#endregion App Functions
 
-  //#region [Todo] Handler Function
-  calculator.HandleOperatorClick = function (self) {
-    if (self.dataset.operator === 'equal') {
-      let tempResult = this.Operate(currUserOperator, calcs[calcs.length - 1].result, currUserVal);
-
-      this.Reset();
-      currUserVal = tempResult;
-      currUserOperator = self.dataset.operator;
-      this.UpdateDisplay(currUserVal);
-    } else if (lastClicked !== 'operator') { // If the user last clicked a number
-      currUserVal = h.Convert2Number(currUserVal);
-
-      // Fisrt calc, set up
-      if (calcs.length === 0) {
-        this.AddCalculation({
-          num1: 0,
-          num2: currUserVal,
-          operator: self.dataset.operator, // currUserOperator will be overwritten add end of function
-          result: this.Operate('', currUserVal)
-        });
-      } else {
-        this.AddCalculation({
-          num1: calcs[calcs.length - 1].result,
-          num2: currUserVal,
-          operator: currUserOperator, // currUserOperator will be overwritten add end of function
-          result: this.Operate(currUserOperator, calcs[calcs.length - 1].result, currUserVal)
-        });
-      }
-
-      currUserVal = '';
-      lastClicked = 'operator';
-      currUserOperator = self.dataset.operator;
-    } else if (lastClicked === 'operator') { // lastClicked is an operator
-      // Auto Invert value 
-      if (currUserOperator === 'add' &&
-        self.dataset.operator === 'subtract') {
-        if (currUserVal === '-')
-          currUserOperator = self.dataset.operator;
-
-        currUserVal = h.ToggleNegativeSign(currUserVal); // Used ToggleNegative before Refactoring
-      }
-      else if (currUserOperator === 'subtract' &&
-        self.dataset.operator === 'subtract') {
-        currUserVal = h.ToggleNegativeSign(currUserVal);
-      } else {
-        currUserOperator = self.dataset.operator;
-        currUserVal = '';
-      }
-    }
-
-    this.UpdateEquation();
-    console.clear();
-    console.table(calcs);
-  }
-  //#endregion Handler Functions
-
-  //#region [Todo] Calculator Functions
+  //#region Calculator Functions
   calculator.Add = (a, b = 0) => a + b;
   calculator.Subtract = (a, b = 0) => a - b;
   calculator.Multiply = (a, b = 1) => a * b;
@@ -170,7 +112,7 @@ export let CalcApp = (function () {
   }
   
   calculator.Operate = function(o, a, b) {
-    switch (o.toLowerCase()) {
+    switch (o) {
       case 'add':
         return this.Add(a, b);
       case 'subtract':
--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@ document.addEventListener("DOMContentLoaded", () => Init());
 
 const _debug = true;
 
-//#region App Functions
 let Init = function () {
-  CalcApp.Init();
+  CalcApp.Init('app', 'display--eval', 'display--equation'); // Pass in ID's for elements used by CalcApp
   SetUpListeners();
 }
-
 
 //#region Handler Function
 let SetUpListeners = () => {
@@ -19,21 +17,23 @@ let SetUpListeners = () => {
   let functionEl = document.querySelectorAll('.btn--function');
 
   numEl.forEach(e => {
-    e.addEventListener('click', HandleNumClick);
+    e.addEventListener('click', (e) => HandleNumClick(e.target.dataset.number));
   });
 
   operatorEl.forEach(e => {
-    e.addEventListener('click', HandleOperatorClick);
+    e.addEventListener('click', (e) => HandleOperatorClick(e.target.dataset.operator));
   });
 
   functionEl.forEach(e => {
-    e.addEventListener('click', HandleFunctionClick);
+    e.addEventListener('click', (e) => HandleFunctionClick(e.target.dataset.function));
   });
 }
 
-// These will control the flow of the Calculator App,
-// CalcApp will control the behind the scenes dark magic stuff
-let HandleNumClick = function () {
+/* 
+ * These will control the flow of the Calculator App,
+ * CalcApp will control the behind the scenes dark magic stuff
+*/
+let HandleNumClick = function (numClicked) {
   // Replace the currUserVal if the user clicks a number
   // right after clicking the equal sign
   let tempVal = (
@@ -41,12 +41,12 @@ let HandleNumClick = function () {
     ) ? '' : h.Convert2String(CalcApp.getCurrUserVal());
 
   // User clicked decimal point
-  if (this.dataset.number === '.') {
+  if (numClicked === '.') {
     if (h.IsFloat(tempVal)) return; // Do nothing if already a decimal
 
     return (tempVal += '.'); // Convert to Decimal if not already one
   } else { // User clicked a number
-    tempVal += this.dataset.number;
+    tempVal += numClicked;
     tempVal = h.Convert2Number(tempVal);
   }
 
@@ -58,19 +58,87 @@ let HandleNumClick = function () {
   if(_debug)
   {
     console.clear();
-    console.warn(this.dataset.number);
+    console.warn(numClicked);
     console.table(CalcApp.getCalcs());
   }
 }
 
-let HandleOperatorClick = function () {
-  CalcApp.HandleOperatorClick(this);
+let HandleOperatorClick = function (latestOp) {
+  let currUsrOp = CalcApp.getCurrUserOperator();
+  let currUsrVal = CalcApp.getCurrUserVal();
+  let lastResult = CalcApp.getLastResult();
+
+  // Fallback
+  let newUsrOp = currUsrOp;
+  let newUsrVal = currUsrVal;
+
+  if (latestOp === 'equal') { // Calculate result no matter what was clicked prior and return out of the function
+    if(currUsrOp === 'equal') return; // Do nothing
+
+    let tempResult = CalcApp.Operate(currUsrOp, lastResult, currUsrVal);
+    newUsrVal = tempResult;
+    newUsrOp = latestOp;
+
+    CalcApp.Reset();
+  }
+  else if (CalcApp.getLastClicked() === 'operator') { // Users last clicked an operator, thus has not clicked a new number to equate on yet
+    newUsrOp = latestOp; // Update the newUsrOp right of the bat
+
+    if(latestOp === 'subtract') {
+      if (currUsrVal === '-') {
+        newUsrVal = '';
+      }
+      else if (currUsrVal === '') {
+        newUsrVal = '-';
+        newUsrOp = currUsrOp; // keep the same operator if only changing the negative sign
+      }
+    }
+  }
+  // User user is clicking an operator for the first time this round
+  else if (CalcApp.getLastClicked() !== 'operator') {
+    newUsrVal = h.Convert2Number(currUsrVal);
+
+    let newCalc = {
+      num1: lastResult,
+      num2: newUsrVal,
+      operator: currUsrOp, // currUserOperator will be overwritten add end of function
+      result: CalcApp.Operate(currUsrOp, lastResult, newUsrVal)
+    };
+
+    // First calc, set up
+    if (CalcApp.isCalcsEmpty()) {
+      newCalc = {
+        ...newCalc,
+        num1: 0,
+        operator: latestOp, // currUserOperator will be overwritten add end of function
+        result: CalcApp.Operate('', newUsrVal)
+      }
+    }
+
+    CalcApp.AddCalculation(newCalc);
+    newUsrVal = '';
+    newUsrOp = latestOp;
+    CalcApp.setLastClicked('operator');
+  }
+
+  CalcApp.setCurrUserVal(newUsrVal);
+  CalcApp.setCurrUserOperator(newUsrOp);
+
+  if(latestOp === 'equal') {
+    CalcApp.UpdateDisplay(CalcApp.getCurrUserVal()); // Updates equation inside
+  }
+  else {
+    CalcApp.UpdateEquation(); // Updates equation inside
+  }
+
+  if (!_debug) {
+    console.clear();
+    console.table(CalcApp.getCalcs());
+  }
 }
 
-let HandleFunctionClick = function () {
-  let funcBtnClicked = this.dataset.function;
-
-  switch (funcBtnClicked) {
+let HandleFunctionClick = function (funcClicked) {
+  switch (funcClicked) {
     case 'clear':
       CalcApp.AllClear();
       break;
